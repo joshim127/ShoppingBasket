059090c8c14618ab929e46935e58cb71be2017d8
Joshim Uddin <JoshimUddin@tfl.gov.uk>
2018-08-25 14:28:31 +0100

    Implemented first exceptance criteria/ first scenario

--- BasketTests/BasketTests.cs
+++ BasketTests/BasketTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ShoppingBasketTests
@@ -19,11 +21,39 @@ namespace ShoppingBasketTests
             var actualTotal = _classUnderTest.Total;
             Assert.AreEqual(0, actualTotal);
         }
+
+        [TestMethod]
+        public void WhenBasketIsNotEmpty_ShouldReturnCorrectTotal()
+        {
+            _classUnderTest.Items.Add(new Product { Price = 100, Description = "Bread", Quantity = 1});
+            _classUnderTest.Items.Add(new Product { Price = 160, Description = "Butter", Quantity = 1});
+            _classUnderTest.Items.Add(new Product { Price = 120, Description = "Milk", Quantity = 1});
+
+            var actualTotal = _classUnderTest.Total;
+            Assert.AreEqual(380, actualTotal);
+
+        }
     }
 
     public class Basket
     {
-        public int Total { get; set; }
+        public Basket()
+        {
+            Items = new List<Product>();
+        }
+
+        public int Total
+        {
+            get { return Items.Sum(x => x.Price * x.Quantity); }
+        }
+
+        public List<Product> Items { get; set; }
     }
 
+    public class Product
+    {
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+    }
 }
